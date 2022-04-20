bcaef9715a11db698b901466da910e3469ba60df
margaritasing <libiamfreitesm@gmail.com>
2022-04-20 15:57:35 -0300

    agrego frontend

---
+++ client/build/precache-manifest.d1009886127fbcb33f559316ba911804.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8314e183e36226c944f0a956e991c974",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4fb6e62b3679479099a",
+    "url": "/static/css/main.e8f3da3b.chunk.css"
+  },
+  {
+    "revision": "16c87acc4e7b36b12178",
+    "url": "/static/js/2.0ca73704.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.0ca73704.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4fb6e62b3679479099a",
+    "url": "/static/js/main.937590d4.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "8bf1af37dd02198b256725e83a203795",
+    "url": "/static/media/cart.8bf1af37.svg"
+  },
+  {
+    "revision": "2c5552a10c3ab0a115c9484f908f982d",
+    "url": "/static/media/close.2c5552a1.svg"
+  },
+  {
+    "revision": "b3f49afdaa769bc2cb267d590e308ae0",
+    "url": "/static/media/menu.b3f49afd.svg"
+  }
+]);
---
+++ frontend/src/components/mainpages/utils/not_found/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NotFound() {
+    return (
+        <div>
+            404 | Not Found | No funciona
+        </div>
+    )
+}
+
+export default NotFound
